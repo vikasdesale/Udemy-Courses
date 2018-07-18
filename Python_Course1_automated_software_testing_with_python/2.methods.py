a42d1eda12810e37f7ca4ecd32aa4ebd7301fefa
vikasdesale <vikasdesaleu@gmail.com>
2018-07-18 13:06:31 +0530

    2. Methods

---
+++ Python_Course1_automated_software_testing_with_python/2.methods.py
@@ -0,0 +1,9 @@
+# Complete the method by making sure it returns 42. .
+def return_42():
+    # Complete method here
+    return 42  # 'pass' just means "do nothing". Make sure to delete this!
+
+# Create a method below, called my_method, that takes two arguments and returns the result of its two arguments multiplied together.
+def my_method(no_one,no_two):
+    return no_one * no_two
+
