21069173a947510e332b4f28054c0f000a0c7f88
vikasdesale <vikas.desaleu@gmail.com>
2018-09-27 20:19:08 +0530

    7.exercise

---
+++ Python_Course1_automated_software_testing_with_python/7.exercise_with_function.py
@@ -0,0 +1,17 @@
+def who_do_you_know():
+	people = input("Enter the names of people you know, separated by commas:")
+	people_list = people.split(",")
+
+	people_without_spaces = []
+	for person in people_list:
+		people_without_spaces.append(person.strip())
+
+
+	return people_without_spaces
+
+def ask_user():
+	person = input("Enter a name of someone you know: ")
+	if person in who_do_you_know():
+		print("You know {}!".format(person))
+
+ask_user()
